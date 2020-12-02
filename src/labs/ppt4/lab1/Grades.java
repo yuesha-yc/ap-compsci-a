b23447cdf88f4950fe9b5cfebe5cebec7d4319ed
yuesha-yc <kevinwangyichen@gmail.com>
2020-12-02 15:46:20 +0800

    PPT4 Lab1

---
+++ src/labs/ppt4/lab1/Grades.java
@@ -0,0 +1,34 @@
+package labs.ppt4.lab1;
+
+// ****************************************************************
+//   Grades.java
+//
+//   Use Student class to get test grades for two students
+//   and compute averages
+//
+// ****************************************************************
+public class Grades
+{
+    public static void main(String[] args)
+    {
+        Student student1 = new Student("Mary");
+        Student student2 = new Student("Mike");
+
+        //input grades for Mary
+        student1.inputGrades();
+        //print average for Mary
+        System.out.println("The average for " + student1.getName() + " is " + student1.getAverage() + ".");
+
+        System.out.println();
+
+        //input grades for Mike
+        student2.inputGrades();
+        //print average for Mike
+        System.out.println("The average for " + student2.getName() + " is " + student2.getAverage() + ".");
+
+        System.out.println();
+        System.out.println("Student 1: " + student1);
+        System.out.println("Student 2: " + student2);
+
+    }
+}
---
+++ src/labs/ppt4/lab1/Student.java
@@ -0,0 +1,63 @@
+package labs.ppt4.lab1;
+
+// ****************************************************************
+//   Student.java
+//
+//   Define a student class that stores name, score on test 1, and
+//   score on test 2.  Methods prompt for and read in grades,
+//   compute the average, and return a string containing student’s info.
+// ****************************************************************
+import java.util.Scanner;
+
+public class Student
+{
+    //declare instance data
+    private String name;
+    private int score1;
+    private int score2;
+
+    Scanner scan = new Scanner(System.in);
+
+    //-----------------------------------------------
+    //constructor
+    //-----------------------------------------------
+    public Student(String studentName)
+    {
+        name = studentName;
+    }
+
+    //-----------------------------------------------
+    //inputGrades: prompt for and read in student's grades for test1 and test2.
+    //Use name in prompts, e.g., "Enter's Joe's score for test1".
+    //-----------------------------------------------
+    public void inputGrades()
+    {
+        System.out.println("Enter " + name + "'s score for test 1");
+        score1 = scan.nextInt();
+        System.out.println("Enter " + name + "'s score for test 2");
+        score2 = scan.nextInt();
+    }
+
+    //-----------------------------------------------
+    //getAverage: compute and return the student's test average
+    //-----------------------------------------------
+
+    public double getAverage()
+    {
+        return (double) (score1 + score2) / 2;
+    }
+
+    //-----------------------------------------------
+    //getName: print the student's name
+    //-----------------------------------------------
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "  Test1: " + score1 + "  Test2: " + score2;
+    }
+}
