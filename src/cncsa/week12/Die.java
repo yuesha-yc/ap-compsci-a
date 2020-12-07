0cce51fd0873fe570b93c61fa7abd97f97adf381
yuesha-yc <kevinwangyichen@gmail.com>
2020-12-07 15:04:47 +0800

    Week 12 HW

---
+++ src/cncsa/week12/Die.java
@@ -0,0 +1,29 @@
+package cncsa.week12;
+
+public class Die {
+    private final int MAX = 6;
+    private int faceValue;
+
+    public Die() {
+        faceValue = 1;
+    }
+
+    public int roll() {
+        faceValue = (int)(Math.random() * MAX) + 1;
+        return faceValue;
+    }
+
+    public void setFaceValue(int faceValue) {
+        this.faceValue = faceValue;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    @Override
+    public String toString() {
+        String result = Integer.toString(faceValue);
+        return result;
+    }
+}
